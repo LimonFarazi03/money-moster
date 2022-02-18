69bd440ec38ebd987c370be5d43ff2d115423209
Limon Farazi <abidislamabir@gmail.com>
2022-02-18 23:21:05 +0600

    first Commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+document.getElementById('calculate-btn').addEventListener('click',function(){
+  const incomeOutPut = document.getElementById('income-output').value;
+  console.log(incomeOutPut);
+})
