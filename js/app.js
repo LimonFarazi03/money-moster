533ac9ad56ea047409231015d6a0cd27b3560073
Limon Farazi <abidislamabir@gmail.com>
2022-02-19 14:41:09 +0600

    some changes

--- js/app.js
+++ js/app.js
@@ -7,11 +7,17 @@
 
 document.getElementById('calculate-btn').addEventListener('click',function(){
   const tootalCost = parseFloat(foodOutput.value) + parseFloat(rentOutput.value) + parseFloat(clothOutput.value);
+
   if(incomeOutput.value == ''){
-    alert('Your income please')
-  }
-  if(foodOutput.value == '' || rentOutput.value == '' ||clothOutput.value == ''){
-    return alert('Sorry Please fill up')
+    return alert('Your income please')
+  }else if(incomeOutput.value < 0){
+    return alert('Areah! baba negative number jibone income korso')
+  }else if(foodOutput.value == ''){
+    alert('Please fill up Food Field')
+  }else if(rentOutput.value = ''){
+    alert('Please fill up Rent Field')
+  }else if(clothOutput.value == ''){
+    alert('Please fill up Cloth Field')
   }
   const totalExp = (totalExpeOutput.innerText = tootalCost);
   const haveMoney = incomeOutput.value - totalExp;
